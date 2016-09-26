ae9452a88c7d29b398b408847d18d69ff53449da
PvMeira <pv.ciria@gmail.com>
2016-09-25 21:02:25 -0300

    Adicionando validações para CPF e EMAIL

--- WIR/src/com/senac/cl/validadores/ValidadorParaCpf.java
+++ WIR/src/com/senac/cl/validadores/ValidadorParaCpf.java
@@ -1,6 +1,7 @@
 package com.senac.cl.validadores;
 
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
@@ -10,69 +11,54 @@ import javax.faces.validator.ValidatorException;
 
 import com.senac.cl.utilitarios.SistemaDeMensagens;;
 /**
- * 
  * @author Pedro
- * @since 27/08/2016
+ * @since 25/09/2016
  */
 @FacesValidator(value = "cpfValidator")
 public class ValidadorParaCpf implements Validator {
 
-    private Integer firstDigit, secondDigit;
-/**
- * Validao o numero de caracteres do CPF < ou > 11
- */
-    @Override
-    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
-        if (value != null) {
-            String cpf = value.toString();
-            if (cpf.length() < 11) {
-                cpf = String.format("%011d", Long.parseLong(value.toString()));
-            }
-            if (isAPatternCpf(cpf) || !validateCpf(cpf)) {
-                ((UIInput) component).setValid(false);
-                SistemaDeMensagens.notificaERRO("CPF invalido","numero ou caracteres invalidos");
-            }
-        }
-    }
-/**
- * Verifica se Tem a patern de CPF
- * @param cpf
- * @return
- */
-    private static boolean isAPatternCpf(String cpf) {
-        return cpf.matches("\\b(\\d)\\1+\\b");
-    }
-/**
- * Calculo para a validação do CPF
- * @param cpf
- * @return
- */
-    private boolean validateCpf(String cpf) {
-        firstDigit = calculateCheckerOfDigit(10, cpf.substring(0, 9));
-        secondDigit = calculateCheckerOfDigit(11, cpf.substring(0, 9).concat(firstDigit.toString()));
+	private Integer firstDigit, secondDigit;
 
-        return (firstDigit.toString().concat(secondDigit.toString())).equals(cpf.substring(9, 11));
-    }
-/**
- * checa o calculo do digito
- * @param multiplicador
- * @param cpf
- * @return
- */
-    private static Integer calculateCheckerOfDigit(int multiplicador, String cpf) {
-        Integer digit;
-        int soma = 0;
-        for (int i = 0; i < cpf.length(); i++) {
-            soma += (Integer.parseInt(cpf.substring(i, i + 1)) * multiplicador);
-            multiplicador -= 1;
-        }
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		if (value != null) {
+			String cpf = value.toString();
+			if (cpf.length() < 11) {
+				cpf = String.format("%011d", Long.parseLong(value.toString()));
+			}
+			if (isAPatternCpf(cpf) || !validateCpf(cpf)) {
+				((UIInput) component).setValid(false);
+				throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro de validação",
+						cpf + " não é um CPF válido;"));
+			}
+		}
+	}
 
-        if (soma % 11 < 2) {
-            digit = 0;
-        } else {
-            digit = new Integer(11 - (soma % 11));
-        }
-        return digit;
-    }
+	private static boolean isAPatternCpf(String cpf) {
+		return cpf.matches("\\b(\\d)\\1+\\b");
+	}
+
+	private boolean validateCpf(String cpf) {
+		firstDigit = calculateCheckerOfDigit(10, cpf.substring(0, 9));
+		secondDigit = calculateCheckerOfDigit(11, cpf.substring(0, 9).concat(firstDigit.toString()));
+
+		return (firstDigit.toString().concat(secondDigit.toString())).equals(cpf.substring(9, 11));
+	}
+
+	private static Integer calculateCheckerOfDigit(int multiplicador, String cpf) {
+		Integer digit;
+		int soma = 0;
+		for (int i = 0; i < cpf.length(); i++) {
+			soma += (Integer.parseInt(cpf.substring(i, i + 1)) * multiplicador);
+			multiplicador -= 1;
+		}
+
+		if (soma % 11 < 2) {
+			digit = 0;
+		} else {
+			digit = new Integer(11 - (soma % 11));
+		}
+		return digit;
+	}
 
 }
---
+++ WIR/src/com/senac/cl/validadores/ValidadorParaEmail.java
@@ -0,0 +1,39 @@
+package com.senac.cl.validadores;
+
+import javax.faces.validator.FacesValidator;
+
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+/**
+ * @author Pedro
+ * @since 25/09/2016
+ */
+@FacesValidator("emailValidator")
+public class ValidadorParaEmail implements Validator {
+
+	private Pattern pattern;
+
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	public ValidadorParaEmail() {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+	}
+
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		if (value == null) {
+			return;
+		}
+
+		if (!pattern.matcher(value.toString()).matches()) {
+			throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro de validação",
+					value + " não é um email válido;"));
+		}
+	}
+
+}
